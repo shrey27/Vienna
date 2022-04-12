422df3f44c118d5ccc13d5dfab112665c3a7a88a
Shrey Pandey <shreypandey27@gmail.com>
2022-04-12 09:23:54 +0530

    footer updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { availableRoutes } from './frontend/routes';
-import { Navbar } from './frontend/components';
+import { Navbar, Footer } from './frontend/components';
 
 function App() {
   return (
     <div>
       <Navbar />
       {availableRoutes}
+      <Footer />
     </div>
   );
 }
--- src/frontend/components/footer/index.js
+++ src/frontend/components/footer/index.js
@@ -1,5 +1,27 @@
 import './footer.css';
 
 export function Footer() {
-  return <div>A Footer</div>;
+  return (
+    <footer className='footer__ctr flex-ct-ct flex-vertical sm-s'>
+      <span className='footer__text tag md bl mg--half'>Connect With Me</span>
+      <div>
+        <a href='https://twitter.com/home' target='_blank' rel='noreferrer'>
+          <i className='fa-brands fa-twitter'></i>
+        </a>
+        <a href='https://github.com/shrey27' target='_blank' rel='noreferrer'>
+          <i className='fa-brands fa-github'></i>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/shrey27/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <i className='fa-brands fa-linkedin'></i>
+        </a>
+      </div>
+      <span className='footer__text tag sm sb cen mg--half'>
+        © 2022 Acquaints Pvt Ltd. All rights reserved.
+      </span>
+    </footer>
+  );
 }
