bcb562a432cb8d072f2de3e581125b1b1ad3ec49
Shrey Pandey <shreypandey27@gmail.com>
2022-04-13 13:13:33 +0530

    suggested changes done

--- src/frontend/components/sidebar/index.js
+++ src/frontend/components/sidebar/index.js
@@ -3,9 +3,9 @@ import './sidebar.css';
 import { NavLink } from 'react-router-dom';
 import { navlinks } from '../../utility/constants';
 
-// const backgroundStyle = ({ isActive }) => {
-//   return isActive ? `sidebar__options selected` : `sidebar__options`;
-// };
+const backgroundStyle = ({ isActive }) => {
+  return isActive ? `sidebar__options selected` : `sidebar__options`;
+};
 
 export function Sidebar() {
   return (
@@ -13,7 +13,7 @@ export function Sidebar() {
       <div className='sidebar sidefixed'>
         {navlinks.map((elem) => {
           return (
-            <NavLink to={elem?.path} key={elem.id} className='sidebar__options'>
+            <NavLink to={elem?.path} key={elem.id} className={backgroundStyle}>
               <i className={elem.class}></i>
               <span className='sidebar__options__span'>{elem.name}</span>
             </NavLink>
--- src/frontend/pages/homepage/index.js
+++ src/frontend/pages/homepage/index.js
@@ -1,22 +1,7 @@
 import './homepage.css';
 import { Sidebar } from '../../components';
 import { posts } from '../../utility/constants';
-/**
- * 
- * {
-    _id: 'P1',
-    username: 'Carlos',
-    userId: '@carlos123',
-    profilePic: 'https://www.w3schools.com/howto/img_avatar.png',
-    title: 'Lorem Ipsum',
-    description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    banner: '',
-    likes: 3,
-    comments: 3,
-    bookmarked: false
-  }
- */
+
 export default function Homepage() {
   return (
     <div className='main__grid'>
